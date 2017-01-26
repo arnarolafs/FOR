8b47dcccd63dadab01d6760d1cc00987dbd0b167
arnarolafs <arnarol255@nemi.tskoli.is>
2017-01-26 14:43:24 +0000

    liður 3

--- git_for_verkefni.py
+++ git_for_verkefni.py
@@ -20,3 +20,27 @@ fornafn = input("\nsláðu inn fornafn: ")
 eftirnafn = input("sláðu inn eftirnafn: ")
 
 print("Halló %s %s" % (fornafn, eftirnafn))
+
+#liður 3
+lagstafur = 0
+hastafur = 0
+H_eftir_L = 0
+texti = input("Sláðu inn texta: ")
+for c in range(len(texti)):
+    if texti[c].isupper():
+        hastafur += 1
+       if texti[c+1].islower():
+            H_eftir_L += 1
+    if texti[c].islower():
+        lagstafur += 1
+print("Texti: %s" % texti)
+print("Lágstafir: %s" %lagstafur)
+print("Hástafir: %s" %hastafur)
+print("Lágstafur á eftir Hástaf: %s" %H_eftir_L)
+
+
+
+
+
+
+
