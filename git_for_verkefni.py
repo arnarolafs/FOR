1a5298ade86f767e85fb2289f7cb5fe2b87ac2e8
arnarolafs <arnarol255@nemi.tskoli.is>
2017-01-26 14:27:45 +0000

    liður 1

--- git_for_verkefni.py
+++ git_for_verkefni.py
@@ -4,3 +4,13 @@
 
 
 #liður 1
+
+tala1 = input("sláðu inn tölu 1")
+tala2 = input("sláðu inn tölu 2")
+
+summa = tala1 + tala2
+margf = tala1 * tala2
+
+print("\nsumma talnanna er: %d" % summa)
+print("marfjöldun talnanan er: %d " % margf)
+
